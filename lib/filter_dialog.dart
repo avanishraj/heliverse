2d0f720be1ee3d4b843dab4df2340e008dc71c85
Avanish Raj Singh <avanishraj005@gmail.com>
2023-11-10 14:50:21 +0530

    added filter

---
+++ lib/filter_dialog.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class FilterDialog extends StatefulWidget {
+  final Function(bool, bool, bool, bool) onFilterChanged;
+
+  const FilterDialog({Key? key, required this.onFilterChanged}) : super(key: key);
+
+  @override
+  _FilterDialogState createState() => _FilterDialogState();
+}
+
+class _FilterDialogState extends State<FilterDialog> {
+  bool showAvailable = true;
+  bool showNotAvailable = true;
+  bool showMale = true;
+  bool showFemale = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Filter Options"),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CheckboxListTile(
+            title: Text("Available"),
+            value: showAvailable,
+            onChanged: (value) {
+              setState(() {
+                showAvailable = value ?? false;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text("Not Available"),
+            value: showNotAvailable,
+            onChanged: (value) {
+              setState(() {
+                showNotAvailable = value ?? false;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text("Male"),
+            value: showMale,
+            onChanged: (value) {
+              setState(() {
+                showMale = value ?? false;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text("Female"),
+            value: showFemale,
+            onChanged: (value) {
+              setState(() {
+                showFemale = value ?? false;
+              });
+            },
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Close the dialog
+          },
+          child: Text("Close"),
+        ),
+        TextButton(
+          onPressed: () {
+            // Pass the selected filter options back to the parent widget
+            widget.onFilterChanged(showAvailable, showNotAvailable, showMale, showFemale);
+            Navigator.of(context).pop(); // Close the dialog
+          },
+          child: Text("Submit"),
+        ),
+      ],
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,9 +3,10 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as rootBundle;
 import 'package:heliverse/jsonDetails/jsonDetails.dart';
+import 'filter_dialog.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -13,179 +14,172 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<JsonDetails>? allUsers;
-  bool showAvailable = true;
-  TextEditingController searchContrller = TextEditingController();
+  TextEditingController searchController = TextEditingController();
   String? search = "";
+
+  bool showAvailable = true;
+  bool showNotAvailable = true;
+  bool showMale = true;
+  bool showFemale = true;
 
   @override
   Widget build(BuildContext context) {
+    void _showFilterDialog() {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return FilterDialog(
+            onFilterChanged: (bool available, bool notAvailable, bool male, bool female) {
+              setState(() {
+                showAvailable = available;
+                showNotAvailable = notAvailable;
+                showMale = male;
+                showFemale = female;
+              });
+            },
+          );
+        },
+      );
+    }
+
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {},
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          _showFilterDialog();
+        },
+      ),
+      appBar: AppBar(
+        elevation: 20,
+        title: const Text(
+          "Heliverse",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
-        appBar: AppBar(
-          elevation: 20,
-          title: const Text(
-            "Heliverse",
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 5.0,
+              bottom: 5.0,
+              left: 8.0,
+              right: 8.0,
+            ),
+            child: TextField(
+              controller: searchController,
+              decoration: const InputDecoration(
+                hintText: "Search for name",
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (String? value) {
+                print(value);
+                setState(() {
+                  search = value.toString();
+                });
+              },
+            ),
           ),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 5.0, bottom: 5.0, left: 8.0, right: 8.0),
-              child: TextField(
-                controller: searchContrller,
-                decoration: const InputDecoration(
-                    hintText: "Search for name", border: OutlineInputBorder()),
-                onChanged: (String? value) {
-                  print(value);
-                  setState(() {
-                    search = value.toString();
-                  });
-                },
-              ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FilterChip(
+                  label: const Text('Available'),
+                  selected: showAvailable,
+                  onSelected: (bool value) {
+                    setState(() {
+                      showAvailable = value;
+                    });
+                  },
+                ),
+                FilterChip(
+                  label: const Text('Not Available'),
+                  selected: showNotAvailable,
+                  onSelected: (bool value) {
+                    setState(() {
+                      showNotAvailable = value;
+                    });
+                  },
+                ),
+                FilterChip(
+                  label: const Text('Male'),
+                  selected: showMale,
+                  onSelected: (bool value) {
+                    setState(() {
+                      showMale = value;
+                    });
+                  },
+                ),
+                FilterChip(
+                  label: const Text('Female'),
+                  selected: showFemale,
+                  onSelected: (bool value) {
+                    setState(() {
+                      showFemale = value;
+                    });
+                  },
+                ),
+              ],
             ),
-            Expanded(
-              child: FutureBuilder(
-                future: readJsonData(),
-                builder: (context, snapshot) {
-                  if (searchContrller.text.isEmpty && snapshot.hasData) {
-                    var items = snapshot.data as List<JsonDetails>;
-                    return ListView.builder(
-                      itemBuilder: (context, index) {
-                        return Card(
-                          margin: const EdgeInsets.all(5.0),
-                          child: ListTile(
-                            leading: Image(
-                              image:
-                                  NetworkImage(items[index].avatar.toString()),
-                              fit: BoxFit.fill,
-                            ),
-                            title: Text("${items[index].first_name}"),
-                            subtitle: items[index].available == true
-                                ? const Text("Available")
-                                : const Text("Not Available"),
-                          ),
-                        );
-                      },
-                      itemCount: 10,
-                    );
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: readJsonData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("${snapshot.error}"),
+                  );
+                } else if (snapshot.hasData) {
+                  var items = snapshot.data as List<JsonDetails>;
+
+                  // Apply filters
+                  items = items.where((item) {
+                    bool isAvailable = item.available == true && showAvailable;
+                    bool isNotAvailable = item.available == false && showNotAvailable;
+                    bool isMale = item.gender == "Male" && showMale;
+                    bool isFemale = item.gender == "Female" && showFemale;
+
+                    return isAvailable || isNotAvailable || isMale || isFemale;
+                  }).toList();
+
+                  // Apply search filter
+                  if (searchController.text.isNotEmpty) {
+                    items = items.where((item) =>
+                        item.first_name.toString().toLowerCase().contains(searchController.text.toLowerCase()))
+                        .toList();
                   }
-                  if (snapshot.hasError) {
-                    return Center(
-                      child: Text("${snapshot.error}"),
-                    );
-                  } else if (snapshot.hasData) {
-                    var items = snapshot.data as List<JsonDetails>;
-                    return ListView.builder(
-                      itemBuilder: (context, index) {
-                        String name = items[index].first_name.toString().toLowerCase();
-                        if(name.toLowerCase().contains(searchContrller.text.toLowerCase())){
-                          return Card(
-                          margin: const EdgeInsets.all(5.0),
-                          child: ListTile(
-                            leading: Image(
-                              image:
-                                  NetworkImage(items[index].avatar.toString()),
-                              fit: BoxFit.fill,
-                            ),
-                            title: Text("${items[index].first_name}"),
-                            subtitle: items[index].available == true
-                                ? const Text("Available")
-                                : const Text("Not Available"),
+
+                  return ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: const EdgeInsets.all(5.0),
+                        child: ListTile(
+                          leading: Image(
+                            image: NetworkImage(items[index].avatar.toString()),
+                            fit: BoxFit.fill,
                           ),
-                        );
-                        }
-                      },
-                      itemCount: 10,
-                    );
-                  } else {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                },
-              ),
+                          title: Text("${items[index].first_name}"),
+                          subtitle: items[index].available == true
+                              ? const Text("Available")
+                              : const Text("Not Available"),
+                        ),
+                      );
+                    },
+                    itemCount: items.length,
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
-
-  // ignore: non_constant_identifier_names
-  // Card UserCard(List<JsonDetails> items, int index) {
-  //   return Card(
-  //     elevation: 5,
-  //     margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-  //     child: Container(
-  //       padding: const EdgeInsets.all(8.0),
-  //       child: Row(
-  //         crossAxisAlignment: CrossAxisAlignment.center,
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: [
-  //           SizedBox(
-  //             width: 50,
-  //             height: 50,
-  //             child: Image(
-  //               image: NetworkImage(items[index].avatar.toString()),
-  //               fit: BoxFit.fill,
-  //             ),
-  //           ),
-  //           Expanded(
-  //             child: Container(
-  //               padding: const EdgeInsets.only(bottom: 8),
-  //               child: Column(
-  //                 mainAxisAlignment: MainAxisAlignment.center,
-  //                 crossAxisAlignment: CrossAxisAlignment.center,
-  //                 children: [
-  //                   Padding(
-  //                     padding: const EdgeInsets.only(left: 8, right: 8),
-  //                     child: Text("${items[index].first_name}"),
-  //                   )
-  //                 ],
-  //               ),
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
-
-  // void showFilterOptions(BuildContext context) {
-  //   final List<PopupMenuEntry<bool>> filterOptions = [
-  //     const PopupMenuItem<bool>(
-  //       value: true,
-  //       child: Text('Available'),
-  //     ),
-  //     const PopupMenuItem<bool>(
-  //       value: false,
-  //       child: Text('Not Available'),
-  //     ),
-  //   ];
-
-  //   showMenu(
-  //     context: context,
-  //     position: const RelativeRect.fromLTRB(50, 50, 50, 50),
-  //     items: filterOptions,
-  //     elevation: 8.0,
-  //   ).then((value) {
-  //     if (value != null) {
-  //       setState(() {
-  //         showAvailable = value;
-  //       });
-  //     }
-  //   });
-  // }
-
-  // List<JsonDetails> filterUsers() {
-  //   if (showAvailable) {
-  //     return allUsers!.where((user) => user.available!).toList();
-  //   } else {
-  //     return allUsers!;
-  //   }
-  // }
 
   Future<List<JsonDetails>> readJsonData() async {
     final jsondata = await rootBundle.rootBundle
@@ -194,3 +188,8 @@ class _MyHomePageState extends State<MyHomePage> {
     return list.map((e) => JsonDetails.fromJson(e)).toList();
   }
 }
+
+
+
+
+
